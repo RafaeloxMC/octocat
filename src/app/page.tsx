73f805b6b545b1bf680e8b44eb12bb8fd1fbd55b
xvcf <53903394+RafaeloxMC@users.noreply.github.com>
2026-02-13 19:53:31 +0100

    Add main layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,65 +1,48 @@
 import Image from "next/image";
 
 export default function Home() {
-  return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
-  );
+	return (
+		<div className="flex min-h-screen items-center justify-center">
+			<main className="flex min-h-screen w-full max-w-5xl flex-col items-center justify-between py-32 px-16 sm:items-start text-amber-800">
+				<div>
+					<h1 className="text-6xl font-rubik text-start w-full -rotate-12 -translate-x-12 -translate-y-20">
+						Octocats!
+					</h1>
+					<p className="text-xl font-averia">
+						Lorem ipsum dolor sit amet consectetur adipisicing elit.
+						Magnam ratione temporibus, voluptatum totam facere cum,
+						ipsum voluptatem enim fugit dicta impedit? Eius,
+						doloremque. Neque repellendus delectus veniam omnis
+						doloremque magni, ullam, soluta optio distinctio eaque
+						excepturi voluptas vitae similique nobis impedit
+						accusamus quaerat iusto corporis voluptates. Cumque
+						error id blanditiis! Non facilis cumque similique
+						pariatur labore, doloribus tenetur, a est fugit odio
+						fugiat excepturi veniam iusto at reiciendis, repudiandae
+						accusamus necessitatibus explicabo distinctio sapiente
+						repellendus magni commodi ipsum quidem. Libero, eius
+						facilis optio delectus quas nisi assumenda molestiae
+						sapiente temporibus? Accusamus illo fugiat quidem odio
+						unde dolorem rem aperiam ex.
+					</p>
+
+					<div className="grid grid-cols-2 bg-orange-100 rounded-4xl drop-shadow-xl -rotate-6 mt-12 p-4">
+						<Image
+							src={"/octocat_nobg.png"}
+							width={850}
+							height={750}
+							alt={
+								"A picture of GitHub's Octocat plushie called Octoplush"
+							}
+						/>
+						<p className="text-xl font-averia">
+							Lorem ipsum dolor sit amet consectetur adipisicing
+							elit. Molestias eaque consequatur beatae hic
+							voluptas eos iure illo, eveniet sapiente ut.
+						</p>
+					</div>
+				</div>
+			</main>
+		</div>
+	);
 }
---
+++ src/components/grid.tsx
@@ -0,0 +1,15 @@
+import { cn } from "@/lib/utils";
+
+export function GridSmallBackground() {
+	return (
+		<div className="flex h-full w-full items-center justify-center bg-amber-100">
+			<div
+				className={cn(
+					"absolute inset-0",
+					"bg-size-[20px_20px]",
+					"bg-[linear-gradient(to_right,#fee685_1px,transparent_1px),linear-gradient(to_bottom,#fee685_1px,transparent_1px)]",
+				)}
+			/>
+		</div>
+	);
+}
