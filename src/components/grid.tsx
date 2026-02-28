e732b50b951cd4b9e1dd93d0891d21f56fb04a05
xvcf <53903394+RafaeloxMC@users.noreply.github.com>
2026-03-01 00:27:42 +0100

    Set grid background to fixed

--- src/components/grid.tsx
+++ src/components/grid.tsx
@@ -5,7 +5,7 @@ export function GridSmallBackground() {
 		<div className="flex h-full w-full items-center justify-center bg-amber-100">
 			<div
 				className={cn(
-					"absolute inset-0",
+					"fixed inset-0",
 					"bg-size-[20px_20px]",
 					"bg-[linear-gradient(to_right,#fee685_1px,transparent_1px),linear-gradient(to_bottom,#fee685_1px,transparent_1px)]",
 				)}
